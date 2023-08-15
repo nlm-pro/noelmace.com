1abe6d5a1d8e8861dda269ee450f6d50e95bbd6f
NM <nlm-pro@users.noreply.github.com>
2023-08-15 03:13:15 +0200

    refactor: remove theme searchParam
    this feature was disabled by the previous commit, and isn't necessary
    anymore

--- src/home/header.ts
+++ src/home/header.ts
@@ -1,10 +1,9 @@
 import {localized, msg, str} from '@lit/localize';
 import {LitElement, html} from 'lit';
-import {customElement, state} from 'lit/decorators.js';
+import {customElement} from 'lit/decorators.js';
 
 import type {
   ColorSchemeChangeEvent,
-  ColorScheme,
 } from 'dark-mode-toggle/src/dark-mode-toggle';
 
 import 'dark-mode-toggle';
@@ -13,11 +12,12 @@ import '../shared/locale-picker';
 @customElement('nmc-header')
 @localized()
 export class AppHeaderElement extends LitElement {
-  @state()
-  private themeMode: ColorScheme = 'dark';
-
-  private themeColor!: HTMLElement;
-  private msTitleColor!: HTMLElement;
+  private themeColor: HTMLElement = document.querySelector(
+    'meta[name="theme-color"]'
+  )!;
+  private msTitleColor: HTMLElement = document.querySelector(
+    'meta[name="msapplication-TileColor"]'
+  )!;
 
   override render() {
     return html`
@@ -28,7 +28,7 @@ export class AppHeaderElement extends LitElement {
         <dark-mode-toggle
           .light=${msg(str`Light Theme`)}
           .dark=${msg(str`Dark Theme`)}
-          .mode=${this.themeMode}
+          @colorschemechange=${this.toggleTheme}
         ></dark-mode-toggle>
       </header>
     `;
@@ -37,35 +37,6 @@ export class AppHeaderElement extends LitElement {
   // disable shadow-dom as dark-mode can't work inside it
   protected override createRenderRoot() {
     return this;
-  }
-
-  override connectedCallback() {
-    super.connectedCallback();
-
-    this.themeColor = document.querySelector('meta[name="theme-color"]')!;
-    this.msTitleColor = document.querySelector(
-      'meta[name="msapplication-TileColor"]'
-    )!;
-
-    let searchParam = new URL(document.location.toString()).searchParams.get(
-      'theme'
-    );
-
-    if (searchParam === 'dark' || searchParam === 'light') {
-      this.themeMode = searchParam;
-    } else {
-      searchParam = null;
-    }
-
-    document.addEventListener('colorschemechange', this.toggleTheme.bind(this));
-
-    this.toggleTheme(
-      new CustomEvent('color-scheme-change', {
-        detail: {
-          colorScheme: searchParam || this.themeMode,
-        },
-      })
-    );
   }
 
   toggleTheme(e: ColorSchemeChangeEvent) {
